9149a63599ae8af960c638c691c3df9f7363431c
moez <moezabdelkefi17@gmail.com>
2024-09-20 23:11:49 +0100

    fix error

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,4 +1,4 @@
-import prismadb from '@/lib/prismadb';
+import prismadb from "@/lib/prismadb";
 
 export const getTotalRevenue = async (storeId: string) => {
   const paidOrders = await prismadb.order.findMany({
@@ -14,9 +14,10 @@ export const getTotalRevenue = async (storeId: string) => {
       },
     },
   });
+
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber();
+      return orderSum + item.product.price;
     }, 0);
     return total + orderTotal;
   }, 0);
