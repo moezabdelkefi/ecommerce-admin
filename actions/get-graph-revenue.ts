8a08e1a6ef23a2f1d53cc8a3677399cde1f859eb
moez <moezabdelkefi17@gmail.com>
2024-09-20 23:05:50 +0100

    fix error

--- actions/get-graph-revenue.ts
+++ actions/get-graph-revenue.ts
@@ -1,4 +1,4 @@
-import prismadb from '@/lib/prismadb';
+import prismadb from "@/lib/prismadb";
 
 interface GraphData {
   name: string;
@@ -27,23 +27,23 @@ export const getGraphRevenue = async (storeId: string) => {
     let revenueForOrder = 0;
 
     for (const item of order.orderItems) {
-      revenueForOrder += item.product.price.toNumber();
+      revenueForOrder += item.product.price;
     }
     monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
   }
   const graphData: GraphData[] = [
-    { name: 'Jan', total: 0 },
-    { name: 'Feb', total: 0 },
-    { name: 'Mar', total: 0 },
-    { name: 'Apr', total: 0 },
-    { name: 'May', total: 0 },
-    { name: 'Jun', total: 0 },
-    { name: 'Jul', total: 0 },
-    { name: 'Aug', total: 0 },
-    { name: 'Sep', total: 0 },
-    { name: 'Oct', total: 0 },
-    { name: 'Nov', total: 0 },
-    { name: 'Dec', total: 0 },
+    { name: "Jan", total: 0 },
+    { name: "Feb", total: 0 },
+    { name: "Mar", total: 0 },
+    { name: "Apr", total: 0 },
+    { name: "May", total: 0 },
+    { name: "Jun", total: 0 },
+    { name: "Jul", total: 0 },
+    { name: "Aug", total: 0 },
+    { name: "Sep", total: 0 },
+    { name: "Oct", total: 0 },
+    { name: "Nov", total: 0 },
+    { name: "Dec", total: 0 },
   ];
 
   for (const month in monthlyRevenue) {
